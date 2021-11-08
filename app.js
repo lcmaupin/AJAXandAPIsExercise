5959ba09c2b2aaa875066ccbcf1aebd35c48faee
Luke Maupin <lukecmaupin@gmail.com>
2021-11-07 20:56:59 -0500

    Creates index.html, style.css, and app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// AJAX and APIs Exercise
+
+// 1
+const first = document.querySelector('#first');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+first.append(p1);
+first.append(p2);
+
+// 1a
+const jokeJS1 = JSON.parse(`{"id":18,"type":"programming","setup":"Why did the programmer quit his job?","punchline":"Because he didn't get arrays."}`);
+
+// 1b
+
+
+// 2
+const second = document.querySelector('#second');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+second.append(p3);
+second.append(p4);
+
+// 3
+const third = document.querySelector('#third');
+const p5 = document.createElement('p');
+const p6 = document.createElement('p');
+third.append(p5);
+third.append(p6);
+
+// 4
+const fourth = document.querySelector('#fourth');
+const p7 = document.createElement('p');
+fourth.append(p7);
